08c04f7226cfeb1cbaf115b51bebfdc3817285f7
ryan anderson <ryan.anderson@buzzuti.com>
2014-07-24 17:18:29 -0500

    added.

---
+++ src/Core/Utility/MongoExtensions.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+
+namespace EdFiValidation.ApiProxy.Core
+{
+    public static class MongoExtensions
+    {
+        public static MongoCollection<T> GetCollection<T>(this MongoDatabase db)
+        {
+            var tableName = typeof(T).Name;
+            return db.GetCollection<T>(tableName);
+        }
+    }
+}
---
+++ src/Database/MongoExtensions.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+
+namespace Database
+{
+    public static class MongoExtensions
+    {
+        public static MongoCollection<T> GetCollection<T>(this MongoDatabase db)
+        {
+            var tableName = typeof(T).Name;
+            return db.GetCollection<T>(tableName);
+        }
+    }
+}
