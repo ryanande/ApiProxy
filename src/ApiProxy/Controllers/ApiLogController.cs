38628ad4e8839f56b2be8f80891d18af82284ef3
ryan anderson <ryan.anderson@buzzuti.com>
2014-07-23 22:45:44 -0500

    pulled in api log api controller.

---
+++ src/ApiProxy/Controllers/ApiLogController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using AutoMapper;
+using EdFiValidation.ApiProxy.Core.Queries;
+using EdFiValidation.ApiProxy.Models;
+
+namespace EdFiValidation.ApiProxy.Controllers
+{
+    public class ApiLogController : ApiController
+    {
+        private readonly IApiLogItemQueryService _apiLogItemQueryService;
+        public ApiLogController(IApiLogItemQueryService apiLogItemQueryService)
+        {
+            _apiLogItemQueryService = apiLogItemQueryService;
+        }
+
+
+
+        [HttpGet]
+        [Route("~/Sessions/{id}")]
+        public IEnumerable<ApiLogModel> GetSession(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+                return new List<ApiLogModel>();
+
+            var transactions = _apiLogItemQueryService.GetOnSessionId(id);
+            var model = Mapper.Map<List<ApiLogModel>>(transactions);
+            return model;
+        }
+    }
+}
