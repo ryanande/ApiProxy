1c95d290ec030dc92d4baea9f1a586ab6f081569
ryan anderson <ryan.anderson@buzzuti.com>
2014-07-29 11:25:26 -0500

    added UseCase view and controller.

---
+++ src/ApiProxy/Controllers/UseCaseController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AutoMapper;
+using EdFiValidation.ApiProxy.Core.Queries;
+using EdFiValidation.ApiProxy.Models;
+
+namespace EdFiValidation.ApiProxy.Controllers
+{
+    public class UseCaseController : Controller
+    {
+        private readonly IUseCaseQueryService _useCaseQueryService;
+        public UseCaseController(IUseCaseQueryService useCaseQueryService)
+        {
+            _useCaseQueryService = useCaseQueryService;
+        }
+        [System.Web.Mvc.HttpGet]
+        public ActionResult Index()
+        {
+            var transactions = _useCaseQueryService.GetAll();
+            var model = Mapper.Map<List<ApiUseCaseModel>>(transactions);
+            return View(model);
+        }
+
+      
+    }
+}
