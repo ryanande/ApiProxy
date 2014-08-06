aaab64e2bcf5018f3f33dc79bf2b0d5fc2a5278d
ryan anderson <ryan.anderson@buzzuti.com>
2014-08-06 17:53:54 -0500

    added missing file.

---
+++ src/ApiProxy/Controllers/CommandController.cs
@@ -0,0 +1,30 @@
+﻿using EdFiValidation.ApiProxy.Core.Queries;
+using EdFiValidation.ApiProxy.Core.Services;
+using System.Web.Http;
+
+namespace EdFiValidation.ApiProxy.Controllers
+{
+    public class CommandController : ApiController
+    {
+
+        private readonly IRequestResponsePairQueryService _requestResponseRepo;
+        private readonly IUseCaseQueryService _useCaseRepo;
+        private readonly IValidationService _validationService;
+
+        public CommandController(IRequestResponsePairQueryService pairs, 
+                                        IUseCaseQueryService useCaseQueryService, 
+                                        IValidationService validationService)
+        {
+            _requestResponseRepo = pairs;
+            _useCaseRepo = useCaseQueryService;
+            _validationService = validationService;
+        }
+
+        [Route("~/command/validate/{id}")]
+        [HttpGet]
+        public void Execute(string id)
+        {
+            _validationService.Validate(id);
+        }
+    }
+}
